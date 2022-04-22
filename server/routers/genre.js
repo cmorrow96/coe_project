5721423e576c0d5aa20d0c49895b2abd4e7a9305
cmorrow96 <c.morrow212@gmail.com>
2022-04-22 12:32:05 +0100

    refactored db script and controller/router files

--- server/routers/genre.js
+++ server/routers/genre.js
@@ -1,12 +1,32 @@
+const { check } = require("express-validator");
+const { validate } = require("../utils/validation");
+
+const {
+  getGenre,
+  getGenres,
+  createGenre,
+  updateGenre,
+} = require("../controllers/genre");
+
 const { Router } = require("express");
 const router = Router();
 
-router.route("/").get((req, res) => {
-    res.status(200).send("genres");
-})
+router.route("/:id(\\d+)").get(getGenre);
+router.route("/").get(getGenres);
 
-router.route("/").post((req, res) => {
-    res.status(200).send("Example Genre");
-})
+router
+  .route("/")
+  .post(
+    [
+      check("name")
+        .isLength({ min: 3 })
+        .withMessage("the name must have minimum length of 3")
+        .trim(),
+    ],
+    validate,
+    createGenre
+  );
+
+router.route("/:id(\\d+)").put(updateGenre);
 
 module.exports = router;
