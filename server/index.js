e4a26f4c0cf40b37b62d9468845df3092cfd33ee
cmorrow96 <c.morrow212@gmail.com>
2022-06-06 10:22:51 +0100

    Recovered work and changes

--- server/index.js
+++ server/index.js
@@ -9,14 +9,14 @@ const {
   gameRouter,
   authRouter,
 } = require("./routers/index");
-const {verifyToken} = require("./middleware/auth"); 
+const {verifyToken} = require("./middleware/auth");
 const app = express();
 
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
-app.all("*", verifyToken);
+// app.all("*", verifyToken);
 
 app.use("/genres", genreRouter);
 app.use("/developers", developerRouter);
@@ -24,6 +24,10 @@ app.use("/publishers", publisherRouter);
 app.use("/users", userRouter);
 app.use("/games", gameRouter);
 app.use("/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send(err);
+});
 
 app.listen(3001, () => {
   console.log("Server running...");
